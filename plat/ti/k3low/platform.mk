4466a5a621a4e75d8f12c1b7d7a63f534f6da466
Dhruva Gole <d-gole@ti.com>
2024-12-23 23:58:13 +0530

    feat(k3low): support SCMI protocol on k3low
    Add SCMI protocol support for the TI k3low devices like the AM62L which
    will require it for allowing HLOSes to request for clock and power
    domain operations in the future.
    
    Here, add only the base protocol support which shall allow scmi to
    probe from linux/u-boot and thus establish an entry path via scmi to
    TF-A.
    
    Change-Id: I78d192d0cf1b4efe5843254739b32807dee015a1
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

--- plat/ti/k3low/platform.mk
+++ plat/ti/k3low/platform.mk
@@ -12,6 +12,8 @@ include ${PLAT_PATH}/board/${TARGET_BOARD}/board.mk
 include drivers/ti/clk/ti_clk.mk
 include drivers/ti/pd/ti_pd.mk
 include ${PLAT_PATH}/common/pm/ti_soc_pm.mk
+include plat/ti/common/scmi/ti_scmi.mk
+
 
 BL32_BASE ?= 0x80200000
 $(eval $(call add_define,BL32_BASE))
@@ -41,14 +43,17 @@ endif
 CONFIG_CLK_PLL_16FFT_FRACF_CALIBRATION := 1
 $(eval $(call add_define,CONFIG_CLK_PLL_16FFT_FRACF_CALIBRATION))
 
-PLAT_INCLUDES		+= \
-				-I${PLAT_PATH}/board/${TARGET_BOARD}/include \
-				-I${PLAT_PATH} \
-				-Iplat/ti/common/include \
-				-I${PLAT_PATH}/common/drivers/firewall \
-				-I${PLAT_PATH}/common/drivers/k3-ddrss \
-				-I${PLAT_PATH}/common/drivers/k3-ddrss/common \
-				-I${PLAT_PATH}/common/drivers/k3-ddrss/16bit \
+PLAT_INCLUDES +=	\
+			-I${PLAT_PATH}/common/drivers/firewall \
+			-I${PLAT_PATH}/common/drivers/k3-ddrss \
+			-I${PLAT_PATH}/common/drivers/k3-ddrss/common \
+			-I${PLAT_PATH}/common/drivers/k3-ddrss/16bit \
+			-I${PLAT_PATH}/board/${TARGET_BOARD}/include	\
+			-I${PLAT_PATH}					\
+			-Idrivers/scmi-msg				\
+			-Iplat/ti/common/include			\
+			-Iplat/ti/common/scmi				\
+			-Iplat/ti/common/include \
 
 K3_LPDDR4_SOURCES	+= \
 				${PLAT_PATH}/common/drivers/k3-ddrss/am62l_ddrss.c \
@@ -66,11 +71,15 @@ K3_TI_SCI_TRANSPORT	:= \
 BL31_SOURCES		+= \
 				drivers/delay_timer/delay_timer.c \
 				drivers/delay_timer/generic_delay_timer.c \
-				${K3_PSCI_SOURCES} \
-				${K3_TI_SCI_TRANSPORT} \
+				drivers/scmi-msg/base.c		\
+				drivers/scmi-msg/entry.c	\
+				drivers/scmi-msg/smt.c		\
+				${K3_PSCI_SOURCES}		\
+				${K3_TI_SCI_TRANSPORT}		\
 				${PLAT_PATH}/common/am62l_bl31_setup.c \
 				${PLAT_PATH}/common/am62l_topology.c \
 				${PLAT_PATH}/common/drivers/firewall/firewall_config.c \
+				plat/ti/common/k3_svc.c		\
 
 BL1_SOURCES		+= \
 				${PLAT_PATH}/common/am62l_bl1_setup.c \
