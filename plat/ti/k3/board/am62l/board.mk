23b06f2b4ff4b4bbccd9608c37638fbbb3d0fe05
Dhruva Gole <d-gole@ti.com>
2025-01-03 11:51:57 +0530

    feat(ti): Add support for scmi power and clock mgmt
    Add scmi_pd,clk drivers which are responsible for the scmi protocol handling
    for the clock/ power operation requests coming from SCMI client.
    
    Also, Add the pd and clk arrays for AM62L into it's own folder so
    that the scmi drivers can be generic enough and pick the soc specific
    arrays from the soc folder data files.
    
    Change-Id: I1e07b1c8423d3f273552caeda51f3225b75e1419
    Co-developed-by: Kamlesh Gurudasani <kamlesh@ti.com>
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

---
+++ plat/ti/k3/board/am62l/board.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2024, Texas Instruments Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# We dont have system level coherency capability
+USE_COHERENT_MEM	:=	0
+K3_TI_SCI_MAILBOX	:=	1
+$(eval $(call add_define,K3_TI_SCI_MAILBOX))
+
+K3_TI_SCI_TRANSPORT    =      ${PLAT_PATH}/common/drivers/mailbox/mailbox.c
+K3_PSCI_SOURCES		+=	\
+				${PLAT_PATH}/common/am62l_psci.c	\
+
+BL32_BASE ?= 0x80200000
+$(eval $(call add_define,BL32_BASE))
+
+K3_HW_CONFIG_BASE ?= 0x88000000
+$(eval $(call add_define,K3_HW_CONFIG_BASE))
+
+include ${PLAT_PATH}/common/drivers/scmi/ti_scmi.mk
+include ${PLAT_PATH}/common/drivers/pm/pm.mk
+include ${PLAT_PATH}/board/am62l/pm/soc_pm.mk
+
+PLAT_INCLUDES += -Iplat/ti/k3/board/${TARGET_BOARD}/pm			\
+		 -I${PLAT_PATH}/board/am62l/scmi			\
+
+BL31_SOURCES		+=	\
+				${PLAT_PATH}/common/k3_svc.c		\
+				drivers/scmi-msg/base.c			\
+				drivers/scmi-msg/entry.c			\
+				drivers/scmi-msg/smt.c			\
+				drivers/scmi-msg/clock.c			\
+				drivers/scmi-msg/power_domain.c		\
