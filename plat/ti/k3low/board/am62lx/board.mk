5513dcd2e96912e6fed4c6a7ee784844866db469
Kamlesh Gurudasani <kamlesh@ti.com>
2025-01-22 18:53:43 +0530

    feat(k3low): add board specific power management drivers and data
    Add AM62L specific clock and power domain management drivers and
    data.
    
    Change-Id: I3074801f669d24c0ab5c50f58996071ad6eed9eb
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

--- plat/ti/k3low/board/am62lx/board.mk
+++ plat/ti/k3low/board/am62lx/board.mk
@@ -18,3 +18,4 @@ $(eval $(call add_define,K3_HW_CONFIG_BASE))
 
 include drivers/ti/clk/ti_clk.mk
 include drivers/ti/pd/ti_pd.mk
+include ${PLAT_PATH}/board/am62lx/pm/ti_soc_pm.mk
---
+++ plat/ti/k3low/common/pm/ti_host_idx_mapping.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025-2026 Texas Instruments Incorporated - https://www.ti.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include <ti_host_idx_mapping.h>
+#include <ti_hosts.h>
+
+const uint8_t soc_host_indexes[1] = {
+	[TI_HOST_ID_TIFS] = 0U,
+};
+const uint8_t soc_host_indexes_count = (uint8_t)sizeof(soc_host_indexes);
