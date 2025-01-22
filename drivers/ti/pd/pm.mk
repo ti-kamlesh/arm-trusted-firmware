d376285c78d1b5cf6518de4079cfaeac3c1645cf
Kamlesh Gurudasani <kamlesh@ti.com>
2025-01-22 18:55:44 +0530

    feat(ti): enable power management drivers for AM62L
    Enable drivers for clock and power domain management for AM62L,
    so that these can be used by SCMI calls to control clocks
    and power domains.
    
    While at it, create a Makefile which enables power management
    drivers all at once i.e clock and power domain drivers along with all
    the helper functions.
    
    Change-Id: I9affa3257fe3f29ca24c7118bca19156218f0600
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

---
+++ drivers/ti/pd/pm.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2025 Texas Instruments Incorporated - https://www.ti.com
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+PLAT_INCLUDES += -I${PLAT_PATH}/common/drivers/pm/include/
+
+include drivers/ti/clk/ti_clk.mk
+include ${PLAT_PATH}/common/pm/device/device.mk
+
