843c51e4b7fb16334e2ef5df41022bf44883ba5d
Kamlesh Gurudasani <kamlesh@ti.com>
2025-11-20 13:49:16 +0530

    feat(ti): add TI generic PLL driver and control
    Add generic PLL driver providing common PLL operations and utilities
    shared across PLL variants. Includes PLL control driver for low level
    register access and state management.
    
    Change-Id: I3b4cb4f3899f18e6c9ba773c62ee7afa907c2ac8
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

---
+++ drivers/ti/common/pm/include/ti_devgrps.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2025-2026 Texas Instruments Incorporated - https://www.ti.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * Device Group Types
+ *
+ * This header defines the device group (devgrp) type and constants used
+ * to categorize and distinguish device groups in power management operations.
+ */
+
+#ifndef TI_DEVGRPS_H
+#define TI_DEVGRPS_H
+
+#include <stdint.h>
+
+
+/*
+ * Defines the power domain DEVGRP type. Used in code or data
+ * structures that require distinction of devgrps.
+ */
+typedef uint8_t devgrp_t;
+
+/* SoC defined SYSFW devgrp 00 */
+#define TI_DEVGRP_00 ((0x01U) << 0U)
+
+#endif /* TI_DEVGRPS_H */
