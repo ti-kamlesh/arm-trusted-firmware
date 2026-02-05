0dbb4e7874c926219bfe2dec47dbae41f64805e7
Kamlesh Gurudasani <kamlesh@ti.com>
2026-02-05 17:07:23 +0530

    feat(ti): add device group filtering to clock initialization
    Add device group (devgrp) filtering to ti_clk_init() to optimize
    clock initialization by limiting it based on enabled device groups.
    
    The filtering logic iterates through the device group table and stops
    clock initialization at the first disabled device group. This reduces
    boot time by skipping initialization of clocks associated with
    disabled device groups.
    
    Change-Id: Ie8d6beab811a873f794052822dab52d8f642b021
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

--- drivers/ti/clk/ti_clk.c
+++ drivers/ti/clk/ti_clk.c
@@ -25,6 +25,7 @@
 #include <ti_atomic.h>
 #include <ti_clk_mux.h>
 #include <ti_container_of.h>
+#include <ti_device.h>
 
 uint32_t ti_clk_value_set_freq(struct ti_clk *clkp, uint32_t target_hz,
 			    uint32_t min_hz,
@@ -386,9 +387,23 @@ int32_t ti_clk_init(void)
 {
 	bool progress;
 	bool contents;
+	bool enabled = true;
 	int32_t last_error = 0;
 	uint32_t i;
 	uint32_t clock_count = soc_clock_count;
+
+	for (i = 0U; i < soc_devgroup_count; i++) {
+		devgrp_t devgrp;
+
+		/* Translate compressed internal representation to bitfield */
+		devgrp = (devgrp_t) BIT(i - 1U);
+
+		/* First disabled devgroup, stop at this clock index */
+		if (enabled && !ti_pm_devgroup_is_enabled(devgrp)) {
+			clock_count = soc_devgroups[i].clk_idx;
+			enabled = false;
+		}
+	}
 
 	contents = false;
 	progress = false;
