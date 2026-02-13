e1b963128d6a217ea475a1c40d370f55178644b6
Kamlesh Gurudasani <kamlesh@ti.com>
2026-02-13 14:48:22 +0530

    feat(ti): add clock operation request handler
    Add clock operation request hanler for TI K3 platforms.
    This processes clock operation requests from SCMI protocol layer.
    
    Change-Id: I7a4b261cbffe1d096a5fb045dc255f5ae169c865
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

---
+++ drivers/ti/clk/include/ti_clk_handler.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2025-2026 Texas Instruments Incorporated - https://www.ti.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*
+ * TI Clock Handler API Header
+ *
+ * This header defines the SCMI clock protocol handler interface for TI SoCs.
+ * It provides the API functions called by the SCMI layer to handle clock
+ * operations from the host, including initialization and protocol handler
+ * registration.
+ */
+
+#ifndef TI_CLK_HANDLER_H
+#define TI_CLK_HANDLER_H
+
+/**
+ * ti_clk_handler_init() - Initialize the clock handler and register clk_ops
+ *
+ * Registers the TI clock operations with the generic clock framework.
+ * Must be called before using the clock API.
+ */
+void ti_clk_handler_init(void);
+
+#endif /* TI_CLK_HANDLER_H */
