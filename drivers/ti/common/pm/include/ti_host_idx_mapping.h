0f34884a0cef2af2297fe714a150c4cb264bc83d
Kamlesh Gurudasani <kamlesh@ti.com>
2026-02-16 20:15:55 +0530

    feat(ti): add TI device preparation and validation driver
    Add device preparation and validation driver for TI K3 platforms.
    This validates device access and ownership before power management
    operations.
    
    Change-Id: I4f9eada93276d158698178d9c338ac7cf1f9d6f1
    Signed-off-by: Kamlesh Gurudasani <kamlesh@ti.com>

---
+++ drivers/ti/common/pm/include/ti_host_idx_mapping.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2025-2026 Texas Instruments Incorporated - https://www.ti.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef TI_HOST_IDX_MAPPING_H
+#define TI_HOST_IDX_MAPPING_H
+
+#include <stdint.h>
+
+#define TI_HOST_IDX_NONE 255U
+
+extern const uint8_t soc_host_indexes[];
+extern const uint8_t soc_host_indexes_count;
+
+static inline uint8_t ti_host_idx_lookup(uint8_t host_id)
+{
+	return (host_id < soc_host_indexes_count) ? soc_host_indexes[host_id] : TI_HOST_IDX_NONE;
+}
+
+#endif /* TI_HOST_IDX_MAPPING_H */
