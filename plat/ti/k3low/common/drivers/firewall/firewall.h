c9588a50111fba9eaf0731047c4b8954c99e026e
Suhaas Joshi <s-joshi@ti.com>
2026-02-23 15:50:30 +0530

    feat(k3low): am62lx: Firewall BL31 and BL32 memory regions
    Firewall BL31 and BL32 memory regions, such that only secure cores can read and
    write to them. Doing so ensures that even if the non-secure world is
    compromised, the secure world will remain inaccessible.
    
    Change-Id: Iab5dd94c1b9bc80535a78fb2b27b369c6966295b
    Signed-off-by: Suhaas Joshi <s-joshi@ti.com>

--- plat/ti/k3low/common/drivers/firewall/firewall.h
+++ plat/ti/k3low/common/drivers/firewall/firewall.h
@@ -15,9 +15,27 @@
 #define A53_PRIV_ID		4U
 #define FW_BACKGROUND_BIT	BIT(8U)
 
+#define DDR_BASE		0x80000000
+#define DDR_SIZE		0x7FFFFFFF
+
 /* Firewall IDs */
 #define OSPI_FWL_ID		97U
 #define ADC_MCASP_FWL_ID	160U
+#define DDR_FWL_ID		1U
+
+/* Firewall Regions */
+#define DDR_BG_REGION		0U
+#define DDR_BL31_REGION		1U
+#define DDR_BL32_REGION		2U
+
+/* Firewall Control Register Values */
+#define FWL_CTRL_EN		0xA
+#define FWL_CTRL_EN_BG		(FWL_CTRL_EN | FW_BACKGROUND_BIT)
+
+/* Firewall permission values */
+#define FWL_PERM_ALL_RW		0xC3BBBB /* RW access to ALL hosts */
+#define FWL_PERM_SEC_RW		0x400BB /* RW access to only secure hosts */
+
 
 /* Number of firewall regions */
 #define OSPI_FWL_NUM_REGIONS		8U
--- plat/ti/k3low/common/drivers/firewall/firewall_config.c
+++ plat/ti/k3low/common/drivers/firewall/firewall_config.c
@@ -7,6 +7,8 @@
 #include <common/debug.h>
 #include <ti_sci.h>
 #include <ti_sci_protocol.h>
+#include <common/bl_common.h>
+#include <platform_def.h>
 
 #include "firewall.h"
 
@@ -72,8 +74,33 @@ static void remove_fwl_configs(struct fwl_data fwl, enum k3_fwl_region_type fwl_
 	}
 }
 
+static void add_fwl_configs(const uint16_t fwl_id, const uint16_t region,
+			    const uint32_t n_perm_regs, const uint32_t control,
+			    const uint32_t permissions[],
+			    const uint64_t start_address, const uint64_t end_address)
+{
+	uint8_t owner_index = TFA_HOST_ID;
+	uint8_t owner_privid = A53_PRIV_ID;
+	uint16_t owner_permission_bits = 0;
+	int ret = 0;
+
+	ret = ti_sci_change_fwl_owner(fwl_id, region, owner_index,
+				      &owner_privid, &owner_permission_bits);
+	if (ret) {
+		ERROR("Could not change firewall owner (%d)\n", ret);
+	}
+
+	ret = ti_sci_set_fwl_region(fwl_id, region, n_perm_regs,
+				    control, permissions, start_address, end_address);
+	if (ret) {
+		ERROR("Could not disable firewall region information (%d)\n", ret);
+		panic();
+	}
+}
+
 void update_fwl_configs(void)
 {
+	uint32_t permissions[3] = {0};
 	/*
 	 * Disable firewalls that were configured by ROM for boot phase.
 	 *
@@ -88,4 +115,17 @@ void update_fwl_configs(void)
 		remove_fwl_configs(fwls[i], K3_FWL_REGION_FOREGROUND);
 		remove_fwl_configs(fwls[i], K3_FWL_REGION_BACKGROUND);
 	}
+
+	/* Configure background firewall for whole of DDR */
+	permissions[0] = permissions[1] = permissions[2] = FWL_PERM_ALL_RW;
+	add_fwl_configs(DDR_FWL_ID, DDR_BG_REGION, 3, FWL_CTRL_EN_BG,
+			permissions, DDR_BASE, DDR_BASE + DDR_SIZE);
+
+	/* Configure foreground firewall for ATF and OP-TEE */
+	permissions[0] = permissions[1] = permissions[2] = FWL_PERM_SEC_RW;
+	add_fwl_configs(DDR_FWL_ID, DDR_BL31_REGION, 3, FWL_CTRL_EN, permissions,
+			BL31_START, BL31_END);
+	add_fwl_configs(DDR_FWL_ID, DDR_BL32_REGION, 3, FWL_CTRL_EN, permissions,
+			BL32_BASE, BL32_BASE + BL32_SIZE);
+
 }
