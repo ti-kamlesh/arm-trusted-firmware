60f1b526a2cca2b0c1345aa827745244b022d6e6
Kamlesh Gurudasani <kamlesh@ti.com>
2026-03-29 17:16:35 +0530

    fixup! feat(ti): add device state request handler
    Change-Id: I9b7ea747b0fa17724fdde13976e4836f43fd3f67

--- drivers/ti/pd/include/ti_device_handler.h
+++ drivers/ti/pd/include/ti_device_handler.h
@@ -15,7 +15,7 @@
 #ifndef TI_DEVICE_HANDLER_H
 #define TI_DEVICE_HANDLER_H
 
-/**
+/*
  * Claim a device exclusively. When passed with STATE_RETENTION or STATE_ON,
  * it will claim the device exclusively. If another host already has this
  * device set to STATE_RETENTION or STATE_ON, the message will fail. Once
@@ -24,39 +24,38 @@
  */
 #define TI_DEVICE_EXCLUSIVE	      BIT(10)
 
-/**
+/*
  * Turn device off when possible. This must be used in conjunction with
  * SoC dependencies to identify the overall power domain state being
  * achieved.
  */
 #define TI_DEVICE_SW_STATE_AUTO_OFF    0
 
-/** Disable device but keep in retention. */
+/* Disable device but keep in retention. */
 #define TI_DEVICE_SW_STATE_RETENTION   1
 
-/** Turn device on for usage. */
+/* Turn device on for usage. */
 #define TI_DEVICE_SW_STATE_ON	    2
 
-/**
+/*
  * Hardware state indicating device is on.
  */
 #define TI_DEVICE_HW_STATE_ON	    1
 
 /**
- *  \brief  Set Device State Handler PM Function.
+ * ti_set_device_handler() - Set device power state
+ * @dev_id: Device ID to set state for
+ * @enable: true to enable device, false to disable
  *
- *  \param  dev_id   Device ID to set state for
- *  \param  enable   true to enable device, false to disable
- *  \return ret      0 if the API executed successfully.
- *                   EFAIL   if the API failed to execute.
+ * Return: 0 if the API executed successfully, -EFAIL if the API failed
  */
 int32_t ti_set_device_handler(uint32_t dev_id, bool enable);
 
 /**
- *  \brief  Get Device State Handler PM Function.
+ * ti_get_device_handler() - Get device power state
+ * @dev_id: Device ID to get state for
  *
- *  \param  dev_id      Device ID to get state for
- *  \return is_enabled  true if enabled, false if disable
+ * Return: true if enabled, false if disabled
  */
 bool ti_get_device_handler(uint32_t dev_id);
 
